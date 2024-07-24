d5ead846e79ec7e026926083603375a494f0556e
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-07-24 11:20:44 +0300

    cniffer

---
+++ sniffer_demo.py
@@ -0,0 +1,14 @@
+import socket
+import struct
+import textwrap
+
+# unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H' , data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto),data[14:]
+
+#return formatted mac_address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).uppper()
+# 
